e4fe472eb49bcfae0dac6f241366d40be4e7332f
Ahmad Tabash <ahmad2tabash@gmail.com>
2025-09-06 22:19:40 +0300

    fix Filter

--- Forms/frmManagePeople.Designer.cs
+++ Forms/frmManagePeople.Designer.cs
@@ -61,14 +61,17 @@
             this.dgvPeople.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dgvPeople.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPeople.ContextMenuStrip = this.cmsPeopleGrid;
-            this.dgvPeople.Location = new System.Drawing.Point(12, 240);
+            this.dgvPeople.Location = new System.Drawing.Point(14, 295);
+            this.dgvPeople.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvPeople.Name = "dgvPeople";
             this.dgvPeople.ReadOnly = true;
-            this.dgvPeople.Size = new System.Drawing.Size(1206, 362);
+            this.dgvPeople.RowHeadersWidth = 51;
+            this.dgvPeople.Size = new System.Drawing.Size(1407, 446);
             this.dgvPeople.TabIndex = 0;
             // 
             // cmsPeopleGrid
             // 
+            this.cmsPeopleGrid.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.cmsPeopleGrid.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiShowDetails,
             this.toolStripSeparator1,
@@ -79,26 +82,26 @@
             this.tsmiSendEmailToPerson,
             this.tsmiPhoneCallPerson});
             this.cmsPeopleGrid.Name = "cmsPeopleGrid";
-            this.cmsPeopleGrid.Size = new System.Drawing.Size(163, 148);
+            this.cmsPeopleGrid.Size = new System.Drawing.Size(192, 172);
             // 
             // tsmiShowDetails
             // 
             this.tsmiShowDetails.Image = global::DVLD_PresentationLayer.Properties.Resources.PersonDetails_32;
             this.tsmiShowDetails.Name = "tsmiShowDetails";
-            this.tsmiShowDetails.Size = new System.Drawing.Size(162, 22);
+            this.tsmiShowDetails.Size = new System.Drawing.Size(191, 26);
             this.tsmiShowDetails.Text = "Show Details";
             this.tsmiShowDetails.Click += new System.EventHandler(this.tsmiShowDetails_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(159, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(188, 6);
             // 
             // tsmiAddNewPerson
             // 
             this.tsmiAddNewPerson.Image = global::DVLD_PresentationLayer.Properties.Resources.Add_Person_40;
             this.tsmiAddNewPerson.Name = "tsmiAddNewPerson";
-            this.tsmiAddNewPerson.Size = new System.Drawing.Size(162, 22);
+            this.tsmiAddNewPerson.Size = new System.Drawing.Size(191, 26);
             this.tsmiAddNewPerson.Text = "Add New Person";
             this.tsmiAddNewPerson.Click += new System.EventHandler(this.tsmiAddNewPerson_Click);
             // 
@@ -106,7 +109,7 @@
             // 
             this.tsmiEditPerson.Image = ((System.Drawing.Image)(resources.GetObject("tsmiEditPerson.Image")));
             this.tsmiEditPerson.Name = "tsmiEditPerson";
-            this.tsmiEditPerson.Size = new System.Drawing.Size(162, 22);
+            this.tsmiEditPerson.Size = new System.Drawing.Size(191, 26);
             this.tsmiEditPerson.Text = "Edit";
             this.tsmiEditPerson.Click += new System.EventHandler(this.tsmiEditPerson_Click);
             // 
@@ -114,20 +117,20 @@
             // 
             this.tsmiDeletePerson.Image = ((System.Drawing.Image)(resources.GetObject("tsmiDeletePerson.Image")));
             this.tsmiDeletePerson.Name = "tsmiDeletePerson";
-            this.tsmiDeletePerson.Size = new System.Drawing.Size(162, 22);
+            this.tsmiDeletePerson.Size = new System.Drawing.Size(191, 26);
             this.tsmiDeletePerson.Text = "Delete";
             this.tsmiDeletePerson.Click += new System.EventHandler(this.tsmiDeletePerson_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(159, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(188, 6);
             // 
             // tsmiSendEmailToPerson
             // 
             this.tsmiSendEmailToPerson.Image = ((System.Drawing.Image)(resources.GetObject("tsmiSendEmailToPerson.Image")));
             this.tsmiSendEmailToPerson.Name = "tsmiSendEmailToPerson";
-            this.tsmiSendEmailToPerson.Size = new System.Drawing.Size(162, 22);
+            this.tsmiSendEmailToPerson.Size = new System.Drawing.Size(191, 26);
             this.tsmiSendEmailToPerson.Text = "Send Email";
             this.tsmiSendEmailToPerson.Click += new System.EventHandler(this.tsmiSendEmailToPerson_Click);
             // 
@@ -135,7 +138,7 @@
             // 
             this.tsmiPhoneCallPerson.Image = ((System.Drawing.Image)(resources.GetObject("tsmiPhoneCallPerson.Image")));
             this.tsmiPhoneCallPerson.Name = "tsmiPhoneCallPerson";
-            this.tsmiPhoneCallPerson.Size = new System.Drawing.Size(162, 22);
+            this.tsmiPhoneCallPerson.Size = new System.Drawing.Size(191, 26);
             this.tsmiPhoneCallPerson.Text = "Phone Call";
             this.tsmiPhoneCallPerson.Click += new System.EventHandler(this.tsmiPhoneCallPerson_Click);
             // 
@@ -143,9 +146,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 26F);
-            this.label1.Location = new System.Drawing.Point(489, 161);
+            this.label1.Location = new System.Drawing.Point(570, 198);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(250, 42);
+            this.label1.Size = new System.Drawing.Size(317, 53);
             this.label1.TabIndex = 1;
             this.label1.Text = "Manage People";
             // 
@@ -153,9 +157,10 @@
             // 
             this.lblRecord.AutoSize = true;
             this.lblRecord.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.lblRecord.Location = new System.Drawing.Point(12, 610);
+            this.lblRecord.Location = new System.Drawing.Point(14, 751);
+            this.lblRecord.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRecord.Name = "lblRecord";
-            this.lblRecord.Size = new System.Drawing.Size(81, 19);
+            this.lblRecord.Size = new System.Drawing.Size(99, 24);
             this.lblRecord.TabIndex = 3;
             this.lblRecord.Text = "Records : ";
             // 
@@ -163,9 +168,10 @@
             // 
             this.lblRecCount.AutoSize = true;
             this.lblRecCount.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.lblRecCount.Location = new System.Drawing.Point(99, 610);
+            this.lblRecCount.Location = new System.Drawing.Point(115, 751);
+            this.lblRecCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRecCount.Name = "lblRecCount";
-            this.lblRecCount.Size = new System.Drawing.Size(25, 19);
+            this.lblRecCount.Size = new System.Drawing.Size(28, 24);
             this.lblRecCount.TabIndex = 4;
             this.lblRecCount.Text = "??";
             // 
@@ -185,9 +191,10 @@
             "Phone",
             "Email",
             "CountryName"});
-            this.cbFilterBy.Location = new System.Drawing.Point(99, 211);
+            this.cbFilterBy.Location = new System.Drawing.Point(115, 260);
+            this.cbFilterBy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbFilterBy.Name = "cbFilterBy";
-            this.cbFilterBy.Size = new System.Drawing.Size(149, 21);
+            this.cbFilterBy.Size = new System.Drawing.Size(173, 24);
             this.cbFilterBy.TabIndex = 5;
             this.cbFilterBy.SelectedIndexChanged += new System.EventHandler(this.cbFilterBy_SelectedIndexChanged);
             // 
@@ -195,17 +202,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.label2.Location = new System.Drawing.Point(12, 211);
+            this.label2.Location = new System.Drawing.Point(14, 260);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 19);
+            this.label2.Size = new System.Drawing.Size(102, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Filter By : ";
             // 
             // tbFilterBy
             // 
-            this.tbFilterBy.Location = new System.Drawing.Point(264, 211);
+            this.tbFilterBy.Location = new System.Drawing.Point(308, 260);
+            this.tbFilterBy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbFilterBy.Name = "tbFilterBy";
-            this.tbFilterBy.Size = new System.Drawing.Size(127, 20);
+            this.tbFilterBy.Size = new System.Drawing.Size(148, 24);
             this.tbFilterBy.TabIndex = 6;
             this.tbFilterBy.TextChanged += new System.EventHandler(this.tbFilterBy_TextChanged);
             // 
@@ -213,9 +222,10 @@
             // 
             this.rbMale.AutoSize = true;
             this.rbMale.Checked = true;
-            this.rbMale.Location = new System.Drawing.Point(264, 211);
+            this.rbMale.Location = new System.Drawing.Point(308, 260);
+            this.rbMale.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbMale.Name = "rbMale";
-            this.rbMale.Size = new System.Drawing.Size(47, 17);
+            this.rbMale.Size = new System.Drawing.Size(55, 21);
             this.rbMale.TabIndex = 7;
             this.rbMale.TabStop = true;
             this.rbMale.Text = "Male";
@@ -226,9 +236,10 @@
             // rbFeMale
             // 
             this.rbFeMale.AutoSize = true;
-            this.rbFeMale.Location = new System.Drawing.Point(317, 211);
+            this.rbFeMale.Location = new System.Drawing.Point(370, 260);
+            this.rbFeMale.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbFeMale.Name = "rbFeMale";
-            this.rbFeMale.Size = new System.Drawing.Size(59, 17);
+            this.rbFeMale.Size = new System.Drawing.Size(69, 21);
             this.rbFeMale.TabIndex = 7;
             this.rbFeMale.Text = "FeMale";
             this.rbFeMale.UseVisualStyleBackColor = true;
@@ -238,18 +249,19 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::DVLD_PresentationLayer.Properties.Resources.People_400;
-            this.pictureBox1.Location = new System.Drawing.Point(474, 20);
+            this.pictureBox1.Location = new System.Drawing.Point(553, 25);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(283, 138);
+            this.pictureBox1.Size = new System.Drawing.Size(330, 170);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // frmManagePeople
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1230, 668);
+            this.ClientSize = new System.Drawing.Size(1435, 822);
             this.Controls.Add(this.rbFeMale);
             this.Controls.Add(this.rbMale);
             this.Controls.Add(this.tbFilterBy);
@@ -261,6 +273,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvPeople);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmManagePeople";
